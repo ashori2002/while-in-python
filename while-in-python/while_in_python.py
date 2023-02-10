91d4a116b04a1a36ca4b880e486139dae889e069
ashori2002 <mohammadashori2002@gmail.com>
2023-02-10 15:56:27 +0330

    while..............

--- while-in-python/while_in_python.py
+++ while-in-python/while_in_python.py
@@ -1,1 +1,17 @@
 
+
+number = 5
+#while number <10:
+#    print(number)
+#__________________________________________
+while number <10:
+    number+=1
+    print(number)
+#__________________________________________
+
+users=["ali","akbar","reza"]
+
+varifi  = []
+
+while users:
+    user=users.pop()
